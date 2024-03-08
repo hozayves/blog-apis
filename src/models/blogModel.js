b8ebca7901839790b39f868935d986682a2cce2a
hozayves <muhozayves2014@gmail.com>
2024-03-08 21:59:50 +0200

    ft add mongoose validation create a function for handling endpoint request body validation

--- src/models/blogModel.js
+++ src/models/blogModel.js
@@ -1,11 +1,39 @@
+const Joi = require("joi");
 const mongoose = require("mongoose");
+const { User } = require("../models/user");
 
 const BlogSchema = mongoose.Schema(
   {
-    title: { type: String, required: true },
-    story: { type: String, required: true },
-    tags: { type: [String], default: [] },
-    image: { type: String, default: "" },
+    title: {
+      type: String,
+      required: true,
+      minlength: 5,
+      maxlength: 255,
+      lowercase: true,
+      trim: true,
+    },
+    story: {
+      type: String,
+      required: true,
+      lowercase: true,
+      trim: true,
+    },
+    tags: {
+      type: [String],
+      default: [],
+      lowercase: true,
+      trim: true,
+      maxlength: 20,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    likes: {
+      type: [mongoose.Schema.Types.ObjectId],
+      ref: "User",
+    },
   },
   {
     timestamps: true,
@@ -14,6 +42,17 @@ const BlogSchema = mongoose.Schema(
 
 const BlogModel = mongoose.model("Blog", BlogSchema);
 
+function validateBlog(blog) {
+  const schema = Joi.object({
+    title: Joi.string().required().max(255),
+    story: Joi.string().required().max(10240),
+    tags: Joi.string().required().max(20),
+    image: Joi.required(),
+  });
+  return schema.validate(blog);
+}
+
 module.exports = {
   BlogModel,
+  validateBlog,
 };
