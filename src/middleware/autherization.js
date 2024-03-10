494bbc19ab2aec6e2d3df2119bc9e7b60bf3d44e
hozayves <muhozayves2014@gmail.com>
2024-03-10 21:21:00 +0200

    ft authentication middleware function

---
+++ src/middleware/autherization.js
@@ -0,0 +1,18 @@
+const jwt = require("jsonwebtoken");
+const config = require("config");
+
+module.exports = function (req, res, next) {
+  const token = req.header("blog-auth-token");
+  if (!token)
+    return res
+      .status(401)
+      .json({ ok: false, message: "Access denied. No token provided" });
+
+  try {
+    const decodePayload = jwt.verify(token, config.get("jwtPrivateKey"));
+    req.auth = decodePayload;
+    next();
+  } catch (error) {
+    res.status(400).json({ ok: false, message: "Invalid token" });
+  }
+};
