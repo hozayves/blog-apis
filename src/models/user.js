66a55303b5949bbd489f7fe6c106de63244405bc
hozayves <muhozayves2014@gmail.com>
2024-03-10 21:26:44 +0200

    fx refactor user schema

--- src/models/user.js
+++ src/models/user.js
@@ -1,47 +1,56 @@
+const jwt = require("jsonwebtoken");
 const Joi = require("joi");
 const mongoose = require("mongoose");
+const config = require("config");
 
-const User = mongoose.model(
-  "User",
-  new mongoose.Schema({
-    name: {
-      type: String,
-      required: true,
-      maxlength: 50,
-      trim: true,
-      lowercase: true,
-    },
-    email: {
-      type: String,
-      required: true,
-      trim: true,
-      lowercase: true,
-      unique: true,
-    },
-    profile: {
-      type: String,
-      default: null,
-    },
-    region: {
-      type: String,
-      default: "",
-    },
-    gender: {
-      type: String,
-      enum: ["male", "female"],
-      required: true,
-    },
-    admin: {
-      type: Boolean,
-      default: false,
-    },
-    password: {
-      type: String,
-      required: true,
-      maxlength: 1024,
-    },
-  })
-);
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    maxlength: 50,
+    trim: true,
+    lowercase: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    unique: true,
+  },
+  profile: {
+    type: String,
+    default: null,
+  },
+  region: {
+    type: String,
+    default: "",
+  },
+  gender: {
+    type: String,
+    enum: ["male", "female"],
+    required: true,
+  },
+  admin: {
+    type: Boolean,
+    default: false,
+  },
+  password: {
+    type: String,
+    required: true,
+    maxlength: 1024,
+  },
+});
+userSchema.methods.generateAuthToken = function () {
+  const token = jwt.sign(
+    { _id: this.id, admin: this.admin, email: this.email },
+    config.get("jwtPrivateKey"),
+    { algorithm: "HS256" }
+  );
+  return token;
+};
+
+const User = mongoose.model("User", userSchema);
 
 function validateUser(user) {
   const schema = Joi.object({
