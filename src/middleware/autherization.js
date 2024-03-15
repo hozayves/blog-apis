831ce52f379cf1a5b277bbb810a42cc78180ba0f
hozayves <muhozayves2014@gmail.com>
2024-03-15 21:28:21 +0200

    fx token capturing

--- src/middleware/autherization.js
+++ src/middleware/autherization.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 const config = require("config");
 
 module.exports = function (req, res, next) {
-  const token = req.header("blog-auth-token");
+  const token = req.header("blog-auth-token") || req.query.access_token;
   if (!token)
     return res
       .status(401)
