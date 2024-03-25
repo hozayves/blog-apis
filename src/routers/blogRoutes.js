3ca32939f945bb2215b8c8082364271b787c1b6d
hozayves <muhozayves2014@gmail.com>
2024-03-25 22:27:23 +0200

    fx add validObjectId middleware

--- src/routers/blogRoutes.js
+++ src/routers/blogRoutes.js
@@ -10,16 +10,17 @@ const {
   updateBlog,
 } = require("../controllers/blogController");
 const { upload } = require("../middleware/imageStorage");
+const validObjectId = require("../middleware/validObjectId");
 
 // Create a new blog
 router.post("/", upload.single("image"), [auth, authorizeAd], createNewBlog);
 // Get all blogs
 router.get("/", getAllBlog);
 // Get blog based on Id
-router.get("/:id", getBlog);
+router.get("/:id", validObjectId, getBlog);
 // Delete a blog based on id
-router.delete("/:id", [auth, authorizeAd], deleteBlog);
+router.delete("/:id", [validObjectId, auth, authorizeAd], deleteBlog);
 // Update a blog based on Id
-router.put("/:id", [auth, authorizeAd], updateBlog);
+router.put("/:id", [validObjectId, auth, authorizeAd], updateBlog);
 
 module.exports = router;
